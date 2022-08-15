08363676b34ed9ec85aeb2873aaee715fe465617
Rajasekharan Vengalil <avranju@nerdworks.dev>
2022-08-15 21:59:02 +0530

    add load more button

--- pff-web/js/app.js
+++ pff-web/js/app.js
@@ -1,54 +1,52 @@
 const Handlebars = require('handlebars');
 
 const template = Handlebars.compile(
-    document.getElementById('search-results-template').innerHTML
+  document.getElementById('search-results-template').innerHTML
 );
 
 document.addEventListener('DOMContentLoaded', () => {
-    // set focus on the search box
-    const searchInput = document.getElementById('search');
-    searchInput.focus();
+  // set focus on the search box
+  const searchInput = document.getElementById('search');
+  searchInput.focus();
 
-    // handle searching with debounce
-    searchInput.addEventListener('keyup', debounce(search, 500));
-
-    window.addEventListener('scroll', () => {});
+  // handle searching with debounce
+  searchInput.addEventListener('keyup', debounce(search, 500));
 });
 
 async function search() {
-    const search = document.getElementById('search').value;
-    if (search.trim().length > 0) {
-        const params = new URLSearchParams();
-        params.append('q', search);
-        const url = `/search?${params.toString()}`;
-        const response = await fetch(url);
-        const data = await response.json();
-        const html = template(data);
-        document.getElementById('search-results').innerHTML = html;
+  const search = document.getElementById('search').value;
+  if (search.trim().length > 0) {
+    const params = new URLSearchParams();
+    params.append('q', search);
+    const url = `/search?${params.toString()}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const html = template(data);
+    document.getElementById('search-results').innerHTML = html;
 
-        setTimeout(() => {
-            let res = document
-                .querySelector('#search-results')
-                .querySelectorAll('#message');
-            let last = res[res.length - 1];
-        }, 1);
-    }
+    setTimeout(() => {
+      let res = document
+        .querySelector('#search-results')
+        .querySelectorAll('#message');
+      let last = res[res.length - 1];
+    }, 1);
+  }
 }
 
 function debounce(callback, delay) {
-    var timeout = null;
-    return function () {
-        //
-        // if a timeout has been registered before then
-        // cancel it so that we can setup a fresh timeout
-        //
-        if (timeout) {
-            clearTimeout(timeout);
-        }
-        var args = arguments;
-        timeout = setTimeout(function () {
-            callback.apply(null, args);
-            timeout = null;
-        }, delay);
-    };
+  var timeout = null;
+  return function() {
+    //
+    // if a timeout has been registered before then
+    // cancel it so that we can setup a fresh timeout
+    //
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    var args = arguments;
+    timeout = setTimeout(function() {
+      callback.apply(null, args);
+      timeout = null;
+    }, delay);
+  };
 }
